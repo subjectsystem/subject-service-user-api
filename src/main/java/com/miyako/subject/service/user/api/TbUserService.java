7486f331f5f37506be3aac73bf98e1ae8de4ca4a
lanxiao wei <weilanxiao@163.com>
2019-08-07 21:14:31 +0800

    创建用户服务。。

---
+++ src/main/java/com/miyako/subject/service/user/api/TbUserService.java
@@ -0,0 +1,9 @@
+package com.miyako.subject.service.user.api;
+
+import com.miyako.subject.commons.domain.TbStudent;
+
+import java.util.List;
+
+public interface TbUserService{
+    public List<TbStudent> selectAll();
+}
