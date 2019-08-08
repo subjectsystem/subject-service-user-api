a20b21ccccb8803e6186843cfa460a0423aebd2d
lanxiao wei <weilanxiao@163.com>
2019-08-08 21:35:48 +0800

    user服务分页展示

--- src/main/java/com/miyako/subject/service/user/api/TbUserService.java
+++ src/main/java/com/miyako/subject/service/user/api/TbUserService.java
@@ -1,9 +1,17 @@
 package com.miyako.subject.service.user.api;
 
+import com.github.pagehelper.PageInfo;
 import com.miyako.subject.commons.domain.TbStudent;
 
 import java.util.List;
 
 public interface TbUserService{
+
     public List<TbStudent> selectAll();
+
+    public TbStudent selectById(Integer id);
+
+    public Integer insert(TbStudent tbStudent);
+
+    public PageInfo<TbStudent> page(int pageNum, int pageSize);
 }
